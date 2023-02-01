dbbbcb117cc322ed02778426e7763a047370add6
Andrew Cook <andrewjcook93@gmail.com>
2023-02-01 14:22:00 -0600

    document title, icon, header, better stats style, potential nav style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () =>{
     return(
-        <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="picker">Picker</Link>
-                <Link to="stats">Stats</Link>
-                <Link to="about">About</Link>
+        <div id="nav-return">
+            <nav id="nav-main">
+                <Link to="/" className="nav-title">Home</Link>
+                <Link to="picker" className="nav-title">Picker</Link>
+                <Link to="stats" className="nav-title">Stats</Link>
+                <Link to="about" className="nav-title">About</Link>
             </nav>
         </div>
     )
